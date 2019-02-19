f5836627c4e52659df9185f59d184de74991f689
kyleclo <kyleclo@uw.edu>
2019-02-18 23:06:07 -0800

    split semeval2017 script into NER and REL

--- scripts/semeval2017_to_conll2003.py
+++ scripts/semeval2017_to_conll2003.py
@@ -22,29 +22,7 @@ Input:
         T15	Material 257 294	pair of nominally identical specimens
         T16	Task 13 33	theoretical analysis
 
-Output 1:  Entity extraction (CONLL2003 format)
-
-
-Output 2:  Relation classification
-    JSONlines where each JSON is:
-    {
-        "text": "Based on the theoretical analysis , the [[ value of the measuring resistor ]] ...",
-        "label": "Synonym-of",
-        "metadata": [7, 11, 12, 12]
-    }
-    ** only contains sentences with relations; entities are gold wrt their Arg1/Arg2 position; no cross-sentence relations **
-
-Output 2:  Relation extraction
-    ** same format as above, but includes sentences without relations & all entity pairs are attempted as Arg1/Arg2 positions **
-
-
-
-Pseudocode for 1:
-    - sentence-ify & tokenize text & assign char-level span indices to each token
-    - for each sentence:
-        for each token:
-            if within entity, do stuff
-
+Output:  Entity extraction (CONLL2003 format)
 
 """
 
@@ -56,19 +34,16 @@ nlp = spacy.load('en_core_web_md')
 
 from sci_bert.common.span import Span, TokenSpan, MentionSpan, label_sent_token_spans
 
-MIN_NUM_TOKENS_PER_SENT = 5
-
 # each instance is a single NER[split][instance_id] = {'spans': List, 'labels': List}
 NER = {'train': {}, 'dev': {}, 'test': {}}
-RELATIONS = {}
 for split in ['train', 'dev', 'test']:
     print(f'Processing {split}')
-
     ann_dir = f'semeval2017/{split}/'
+
+    # loop over each instance ID in this split
     instance_ids = sorted({os.path.splitext(ann_file)[0] for ann_file in os.listdir(ann_dir)})
     for id in instance_ids:
         print(f'Processing {id}')
-
         ann_file = os.path.join(ann_dir, f'{id}.ann')
         txt_file = ann_file.replace('.ann', '.txt')
 
@@ -78,17 +53,16 @@ for split in ['train', 'dev', 'test']:
             text = ''.join([char if char.strip() != '' else ' ' for char in text])  # normalize whitespaces, such as '\xa0' --> ' '
             spacy_text = nlp(text)
 
-        # no sentence splitting (one long sentence) & tokenize
-        # sent_token_spans = TokenSpan.find_sent_token_spans(text=text, sent_tokens=[[token.text for sent in spacy_text.sents for token in sent if token.text.strip() != '']])
-
         # split sentences & tokenize
         sent_token_spans = TokenSpan.find_sent_token_spans(text=text, sent_tokens=[[token.text for token in sent if token.text.strip() != ''] for sent in spacy_text.sents])
 
-        # load annotated entity mentions
+        # load annotations
         mention_spans = set()
         with open(ann_file, 'r') as f_ann:
             for line in f_ann:
                 tup = line.strip('\n').split('\t')
+
+                # load entity mention
                 if tup[0].startswith('T'):
                     entity_id = tup[0]
                     # note: occasionally data looks really stupid like `Task 400 436;437 453` in `S0167931713005042.ann`
@@ -150,61 +124,3 @@ for split in ['train', 'dev', 'test']:
                 f_out.write('\n')  # new sent
             f_out.write('\n')  # new paper
 
-
-# TODO: stop here; below incomplete relations script
-
-
-
-
-
-# group TokenSpans into sentences
-sents: List[List[TokenSpan]] = []
-sent: List[TokenSpan] = []
-for i, token_span in enumerate(token_spans):
-    if i in index_sent_start_tokens:
-        sents.append(sent)
-        sent: List[TokenSpan] = []
-    sent.append(token_span)
-
-# load annotations
-ID_TO_ENTITY = {}
-E1E2_TO_RELATION = {}
-with open(ann_file, 'r') as f_ann:
-    for line in f_ann:
-        tup = line.strip().split('\t')
-
-        # check if it's an entity
-        if tup[0].startswith('T'):
-            entity_id = tup[0]
-            entity_type, start, stop = tup[1].split(' ')
-            start = int(start)
-            stop = int(stop)
-            entity_text = tup[2]
-            ID_TO_ENTITY[entity_id] = {
-                'type': entity_type,
-                'span': (start, stop),
-                'text': entity_text
-            }
-
-        # or if it's an asymmetric relation
-        elif tup[0].startswith('R'):
-            relation_id = tup[0]
-            relation_type, arg1, arg2 = tup[1].split(' ')
-            tag1, e1 = arg1.split(':')
-            tag2, e2 = arg2.split(':')
-            assert tag1 == 'Arg1' and tag2 == 'Arg2'
-            E1E2_TO_RELATION[(e1, e2)] =
-
-        # or if it's a symmetric relation
-        elif tup[0].startswith('*'):
-
-        # not sure
-        else:
-            raise ValueError('Cant parse this line {}'.format(line))
-
-        E1E2_TO_RELATION[]
-
-
-
-with open(ann_file, 'r') as f_ann:
-    pass
