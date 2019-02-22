a0420a304786cdadba3d533703ecf4e97939d0aa
kyleclo <kyleclo@uw.edu>
2019-02-22 15:25:05 -0800

    fix bug in format of pico data

--- scripts/fix_format_pico_conll2003.py
+++ scripts/fix_format_pico_conll2003.py
@@ -30,14 +30,14 @@ for split in ['train', 'dev', 'test']:
         for filename in filenames:
             paper_id = os.path.splitext(filename)[0]
             with open(os.path.join(old_dirname, split, filename), 'r') as f_old:
-                f_new.write(f'--DOCSTART-- ({paper_id})')
+                f_new.write(f'-DOCSTART- ({paper_id})')
                 f_new.write('\n\n')
                 prev_label = 'O'
                 for line in f_old:
                     token, label = line.strip().split(' ')
                     if prev_label == 'O' and label.startswith('I'):
                         label = 'B-' + label[2:]
-                    f_new.write('\t'.join([token, 'NN', 'O', label]))
+                    f_new.write(' '.join([token, 'NN', 'O', label]))
                     f_new.write('\n')
                     # add extra whitespace to split sentences (hacky sort of)
                     if token == '.' and label == 'O':
