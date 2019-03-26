402cb58d3fa66f23ad4bbdbdfc31a58890320587
kyleclo <kyleclo@uw.edu>
2019-03-25 18:02:59 -0700

    update train script w/ new bert file names

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,25 +1,30 @@
 # Run allennlp training locally
 
-dataset='chemprot'
-task='text_classification'
-config_file=allennlp_config/"$task".jsonnet
+
+#
+# edit these variables before running script
+DATASET='chemprot'
+TASK='text_classification'
+
+
+#
+#
+CONFIG_FILE=allennlp_config/"$TASK".json
 
 SEED=13270
 PYTORCH_SEED=`expr $SEED / 10`
 NUMPY_SEED=`expr $PYTORCH_SEED / 10`
-
 export SEED=$SEED
 export PYTORCH_SEED=$PYTORCH_SEED
 export NUMPY_SEED=$NUMPY_SEED
 
-export BERT_VOCAB=vocab/s2vocab_cased.vocab
-export BERT_WEIGHTS=pytorch_models/s2bert_s2vocab_cased_512.tar.gz
-export IS_LOWERCASE=false
-export TRAIN_PATH=data/$task/$dataset/train.txt
-export DEV_PATH=data/$task/$dataset/dev.txt
-export TEST_PATH=data/$task/$dataset/test.txt
-
+export BERT_VOCAB=bert_vocab/scibert_scivocab_uncased.vocab
+export BERT_WEIGHTS=bert_weights/scibert_scivocab_uncased.tar.gz
+export IS_LOWERCASE=true
+export TRAIN_PATH=data/$TASK/$DATASET/train.txt
+export DEV_PATH=data/$TASK/$DATASET/dev.txt
+export TEST_PATH=data/$TASK/$DATASET/test.txt
 
 export CUDA_VISIBLE_DEVICES=0
 
-python -m allennlp.run train $config_file  --include-package sci_bert -s "$@"
+python -m allennlp.run train $CONFIG_FILE  --include-package scibert -s "$@"
