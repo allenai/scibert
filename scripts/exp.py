7084924de1efbeb0946a82d179de48553b25d39c
kyleclo <kyleclo@uw.edu>
2019-05-16 11:20:44 -0700

    add grad accum batch size to exp.py and improve logging

--- scripts/exp.py
+++ scripts/exp.py
@@ -35,13 +35,13 @@ def main(desc: str, not_dry_run: bool = False):
                     # 'bc5cdr',
                     # 'JNLPBA',
                     # 'sciie',
-                    'chemprot',
-                    'citation_intent',
-                    'mag',
-                    'rct-20k',
-                    'sciie-relation-extraction',
-                    'sci-cite',
-                    # 'ebmnlp',
+                    # 'chemprot',
+                    # 'citation_intent',
+                    # 'mag',
+                    # 'rct-20k',
+                    # 'sciie-relation-extraction',
+                    # 'sci-cite',
+                    'ebmnlp',
                     # 'genia',
                 ]:
         for seed in [
@@ -55,7 +55,7 @@ def main(desc: str, not_dry_run: bool = False):
                     # 16410,
                     # 18210,
                     # 18310,
-                    18410,
+                    # 18410,
                     18510,
                     18610
                     ]:
@@ -75,56 +75,67 @@ def main(desc: str, not_dry_run: bool = False):
                         'scibert_scivocab_cased',
                         ]:
 
-                for with_finetuning in ['_finetune', ]: # , '']:
+                for with_finetuning in [
+                                        '_finetune',
+                                        # ''
+                                        ]:
 
-                    if dataset in ['NCBI-disease', 'bc5cdr', 'JNLPBA', 'sciie']:
-                        task = 'ner'
-                    elif dataset in ['chemprot', 'citation_intent', 'mag', 'rct-20k', 'sciie-relation-extraction', 'sci-cite']:
-                        task = 'text_classification'
-                    elif dataset in ['ebmnlp']:
-                        task = 'pico'
-                    elif dataset in ['genia']:
-                        task = 'parsing'
-                    else:
-                        assert False
+                    for grad_accum_batch_size in [
+                                                  32
+                                                 ]:
 
-                    dataset_size = dataset_sizes[dataset]
-                    # determine casing from model name
-                    if 'uncased' in model:
-                        is_lowercase = 'true'
-                    else:
-                        is_lowercase = 'false'
+                        if dataset in ['NCBI-disease', 'bc5cdr', 'JNLPBA', 'sciie']:
+                            task = 'ner'
+                        elif dataset in ['chemprot', 'citation_intent', 'mag', 'rct-20k', 'sciie-relation-extraction', 'sci-cite']:
+                            task = 'text_classification'
+                        elif dataset in ['ebmnlp']:
+                            task = 'pico'
+                        elif dataset in ['genia']:
+                            task = 'parsing'
+                        else:
+                            assert False
 
-                    # config file
-                    config_file = f'allennlp_config/{task}{with_finetuning}.json'
+                        dataset_size = dataset_sizes[dataset]
+                        # determine casing from model name
+                        if 'uncased' in model:
+                            is_lowercase = 'true'
+                        else:
+                            is_lowercase = 'false'
 
-                    # bert files
-                    bert_vocab = f'/scibert/{model}/vocab.txt'
-                    bert_weights = f'/scibert/{model}/weights.tar.gz'
+                        # config file
+                        config_file = f'allennlp_config/{task}{with_finetuning}.json'
 
-                    # data files
-                    train_path = f'data/{task}/{dataset}/train.txt'
-                    dev_path = f'data/{task}/{dataset}/dev.txt'
-                    test_path = f'data/{task}/{dataset}/test.txt'
+                        # bert files
+                        bert_vocab = f'/scibert/{model}/vocab.txt'
+                        bert_weights = f'/scibert/{model}/weights.tar.gz'
 
-                    print(task, dataset, seed, bert_weights, bert_vocab, train_path)
-                    cmd = ' '.join(['python', 'scripts/run_with_beaker.py',
-                        f'{config_file}',
-                        '--source ds_7jfhvq3h6sad:/scibert/',
-                        '--include-package scibert',
-                        '--env CUDA_DEVICE=0',
-                        f'--env DATASET_SIZE={dataset_size}',
-                        f'--env BERT_VOCAB={bert_vocab}',
-                        f'--env BERT_WEIGHTS={bert_weights}',
-                        f'--env TRAIN_PATH={train_path}',
-                        f'--env DEV_PATH={dev_path}',
-                        f'--env TEST_PATH={test_path}',
-                        f'--env IS_LOWERCASE={is_lowercase}',
-                        f'--env SEED={seed}',
-                        f'--env PYTORCH_SEED={pytorch_seed}',
-                        f'--env NUMPY_SEED={numpy_seed}'])
-                    if not_dry_run:
-                        completed = subprocess.run(cmd, shell=True)
-                        print(f'returncode: {completed.returncode}')
+                        # data files
+                        train_path = f'data/{task}/{dataset}/train.txt'
+                        dev_path = f'data/{task}/{dataset}/dev.txt'
+                        test_path = f'data/{task}/{dataset}/test.txt'
+
+                        cmd = ' '.join(['python', 'scripts/run_with_beaker.py',
+                            f'{config_file}',
+                            '--source ds_7jfhvq3h6sad:/scibert/',
+                            '--include-package scibert',
+                            '--env CUDA_DEVICE=0',
+                            f'--env DATASET_SIZE={dataset_size}',
+                            f'--env BERT_VOCAB={bert_vocab}',
+                            f'--env BERT_WEIGHTS={bert_weights}',
+                            f'--env TRAIN_PATH={train_path}',
+                            f'--env DEV_PATH={dev_path}',
+                            f'--env TEST_PATH={test_path}',
+                            f'--env IS_LOWERCASE={is_lowercase}',
+                            f'--env SEED={seed}',
+                            f'--env PYTORCH_SEED={pytorch_seed}',
+                            f'--env NUMPY_SEED={numpy_seed}',
+                            f'--env GRAD_ACCUM_BATCH_SIZE={grad_accum_batch_size}'
+                        ])
+                        print('\n')
+                        print(cmd)
+
+                        if not_dry_run:
+                            completed = subprocess.run(cmd, shell=True)
+                            print(f'returncode: {completed.returncode}')
 
 plac.call(main, sys.argv[1:])
